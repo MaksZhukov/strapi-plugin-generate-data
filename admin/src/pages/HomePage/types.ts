b46df48ab5b79f901abd5725545429c9a268b418
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-10-07 08:38:01 -0500

    Revert "feat: add text to string types"
    This reverts commit 6c9eab366f83a3a4a67dccc5b737da06e437fb9d.

--- admin/src/pages/HomePage/types.ts
+++ admin/src/pages/HomePage/types.ts
@@ -1,23 +1,22 @@
 export enum AttributeType {
-  Integer = 'integer',
-  String = 'string',
-  Text = 'text',
-  Richtext = 'richtext',
-  Email = 'email',
-  Date = 'date',
-  Media = 'media',
-  Boolean = 'boolean',
-  Enumeration = 'enumeration',
-  Password = 'password',
-  UID = 'uid',
-  Decimal = 'decimal',
-  Relation = 'relation',
-  JSON = 'json',
+	Integer = 'integer',
+	String = 'string',
+	Richtext = 'richtext',
+	Email = 'email',
+	Date = 'date',
+	Media = 'media',
+	Boolean = 'boolean',
+	Enumeration = 'enumeration',
+	Password = 'password',
+	UID = 'uid',
+	Decimal = 'decimal',
+	Relation = 'relation',
+	JSON = 'json',
 }
 
 export type Values = {
-  [key: string]:
-    | { min: number; max: number }
-    | { from: Date; to: Date }
-    | { pageCount: number };
+	[key: string]:
+		| { min: number; max: number }
+		| { from: Date; to: Date }
+		| { pageCount: number };
 } | null;
