6c9eab366f83a3a4a67dccc5b737da06e437fb9d
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-10-07 06:25:39 -0500

    feat: add text to string types

--- admin/src/components/attributeTypeComponents/StringInput/StringInput.tsx
+++ admin/src/components/attributeTypeComponents/StringInput/StringInput.tsx
@@ -9,57 +9,61 @@ import { GeneralProps } from '../types';
 import { AttributeType } from '../../../pages/HomePage/types';
 
 interface Props extends GeneralProps {
-	values: { count: number };
-	onChangeValue: (key: string, field: string) => void;
+  values: { count: number };
+  onChangeValue: (key: string, field: string) => void;
 }
 
 const StringInput = ({
-	attribute,
-	attributeKey,
-	checked,
-	disabled,
-	values,
-	onChangeCheck,
-	onChangeValue,
+  attribute,
+  attributeKey,
+  checked,
+  disabled,
+  values,
+  onChangeCheck,
+  onChangeValue,
 }: Props) => {
-	return (
-		<GridItem col={6}>
-			<Box marginBottom='8px'>
-				<Box marginBottom='12px'>
-					<Checkbox
-						disabled={disabled}
-						onChange={onChangeCheck(attributeKey)}
-						checked={checked}>
-						{`${attributeKey} (Field type: ${
-							attribute.type === AttributeType.String
-								? 'String'
-								: 'Richtext'
-						})`}
-					</Checkbox>
-				</Box>
-				<Flex gap='16px'>
-					<Box flex='1'>
-						<NumberInput
-							name=''
-							disabled={!checked}
-							onValueChange={onChangeValue(attributeKey, 'min')}
-							// @ts-ignore
-							value={values.min}
-							label={`min count words`}></NumberInput>
-					</Box>
-					<Box flex='1'>
-						<NumberInput
-							name=''
-							disabled={!checked}
-							onValueChange={onChangeValue(attributeKey, 'max')}
-							// @ts-ignore
-							value={values.max}
-							label={`max count words`}></NumberInput>
-					</Box>
-				</Flex>
-			</Box>
-		</GridItem>
-	);
+  return (
+    <GridItem col={6}>
+      <Box marginBottom="8px">
+        <Box marginBottom="12px">
+          <Checkbox
+            disabled={disabled}
+            onChange={onChangeCheck(attributeKey)}
+            checked={checked}
+          >
+            {`${attributeKey} (Field type: ${
+              attribute.type === AttributeType.String ||
+              attribute.type === AttributeType.Text
+                ? 'String'
+                : 'Richtext'
+            })`}
+          </Checkbox>
+        </Box>
+        <Flex gap="16px">
+          <Box flex="1">
+            <NumberInput
+              name=""
+              disabled={!checked}
+              onValueChange={onChangeValue(attributeKey, 'min')}
+              // @ts-ignore
+              value={values.min}
+              label={`min count words`}
+            ></NumberInput>
+          </Box>
+          <Box flex="1">
+            <NumberInput
+              name=""
+              disabled={!checked}
+              onValueChange={onChangeValue(attributeKey, 'max')}
+              // @ts-ignore
+              value={values.max}
+              label={`max count words`}
+            ></NumberInput>
+          </Box>
+        </Flex>
+      </Box>
+    </GridItem>
+  );
 };
 
 export default StringInput;
